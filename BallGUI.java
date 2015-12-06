5e7da7db16d53a5cfdd4fce475bc7e1c6b797364
Xavier <xavier.grogan@aol.com>
2015-12-05 22:16:23 -0600

    BallGUI.java
    Starting to figure out a GUI for this thing...

---
+++ BallGUI.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+public class BallGUI extends Canvas
+{
+	public void paint(Graphics window)
+	{
+		window.setColor(Color.BLACK);
+		window.fillOval(100, 100, 600, 600);
+		window.setColor(Color.WHITE);
+		window.fillOval(300, 300, 200, 200);
+	}
+	
+	public static void main(String[] args)
+	{
+		Canvas canvas = new BallGUI();
+		JFrame win = new JFrame("Magic 8-Ball!");
+		win.setSize(800,800);
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		canvas.setBackground(Color.WHITE);
+		win.add(canvas);
+		win.setVisible(true);
+	}
+}
