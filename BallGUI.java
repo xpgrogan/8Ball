97238564a29bcf4f72e3a6fbc14ae689c765118a
Xavier <xavier.grogan@aol.com>
2015-12-05 23:00:47 -0600

    Updated BallGUI.java
    Just added a test string to check for positioning inside the circle.

--- BallGUI.java
+++ BallGUI.java
@@ -1,15 +1,17 @@
 import javax.swing.*;
-
 import java.awt.*;
 
 public class BallGUI extends Canvas
 {
 	public void paint(Graphics window)
 	{
+		String test = "Test Test Test";
 		window.setColor(Color.BLACK);
 		window.fillOval(100, 100, 600, 600);
 		window.setColor(Color.WHITE);
 		window.fillOval(300, 300, 200, 200);
+		window.setColor(Color.BLACK);
+		window.drawString(test, 360, 400);
 	}
 	
 	public static void main(String[] args)
